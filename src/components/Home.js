d2e85b858a4eee7ff3410452cd2bcc0d38a85910
Frank Tao <48634762+kugelblitz2@users.noreply.github.com>
2021-06-26 14:09:02 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,31 @@ import Login from './Login/Login';
 import useToken from './Login/useToken';
 
 export default function Home() {
+    const [formState, setState] = useState(0);
+    // New form - 0
+    const [typeSelected, setType] = useState('Github');
+    const [title, setTitle] = useState();
+    const [first, setFirst] = useState();
+    const [last, setLast] = useState();
+    const [link, setLink] = useState('github.com/');
+    const [linkVal, setLinkVal] = useState();
+
+    const types = [
+        {type: "Github", link: "github.com/"},
+        {type: "Facebook", link: "facebook.com/"},
+        {type: "Instagram", link: "instagram.com/"},
+        {type: "Linkedin", link: "linkedin.com/in/"}
+      ];
+
+      const changeTypeLink = (type) => {
+        setType(type)
+        for (var prop in types) {
+              if (types[prop].type === type){
+                setLink(types[prop].link)
+              }
+          }
+      }
+
     return(
         <div class="content">
             <div class="leftbox box">
@@ -38,7 +63,69 @@ export default function Home() {
                 </div>
             </div>
             <div class="rightbox box">
-                git hub
+                
+            <form class="w-5/6 max-w-md p-6">
+          <p class="text-4xl">Configure</p>
+          <div class="flex flex-wrap -mx-3 mb-6 mt-6">
+            <div class="w-full px-3">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+                Profile
+              </label>
+              
+              <div class="relative">
+                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                id="grid-type" onChange={e => (
+                  changeTypeLink(e.target.value)
+                  )} value={typeSelected}>
+                  {types.map(function(type, i) {
+                    return <option key={i}>{type.type}</option>
+                  })}
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-wrap -mx-3 mb-6 mt-6">
+            <div class="w-full px-3">              
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+                Color 1
+              </label>
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+              id="grid-first-name" type="text" placeholder="F9F9F9"
+              onChange={e => setFirst(e.target.value)} value={first}/>
+            </div>
+          </div>
+
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+                Color 2
+              </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+              id="grid-link" type="text" placeholder="#696969"
+              onChange={e => setLinkVal(e.target.value)} value={linkVal}/>
+            </div>
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+                  Accent Color
+              </label>
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+              id="grid-link" type="text" placeholder="#696969"
+              onChange={e => setLinkVal(e.target.value)} value={linkVal}/>
+            </div>
+  
+          </div>
+          <button class="bg-white text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+              <span class="mr-2">{formState ? <p>Update</p> : <p>Add</p>}</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path fill="currentcolor" d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
+            </svg>
+          </button> 
+        </form>
+                
             </div>
         </div>
     )
