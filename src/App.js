50cc235f0cb7cab9efe00dfb2821396922f86239
Frank Tao <48634762+kugelblitz2@users.noreply.github.com>
2021-06-26 01:47:01 -0400

    Changes to main page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
-import './css/index.css';
+import './css/App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-import { LayoutOne, LayoutTwo } from './layouts/layouts';
-import { One, Two, Three, Four } from './components/components';
-import Login from './components/Login/Login';
 
+import { LayoutLeft, LayoutRight } from './layouts/layouts';
+import { Home, Profiles } from './components/components';
+
+import Login from './components/Login/Login';
 import Logo from './assets/logo.png'
 import useToken from './components/Login/useToken';
 
@@ -24,9 +25,8 @@ export default function App() {
   return (
     <Router>
       <header>
-        <img src={Logo} alt="logo"></img>
-
-        <div class="bg-gray-200 text-sm text-gray-500 leading-none border-2 border-gray-200 rounded-full inline-flex">
+        <img class="headelement logo" src={Logo} alt="logo"></img>
+        <div class="headelement modetoggle bg-gray-200 text-sm text-gray-500 leading-none border-2 border-gray-200 rounded-full inline-flex">
           <button class="inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 focus:text-blue-400 rounded-l-full px-4 py-2 active" id="grid">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="fill-current w-4 h-4 mr-2"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
             <span>Grid</span>
@@ -36,41 +36,26 @@ export default function App() {
             <span>List</span>
           </button>
         </div>
-        
-      </header>
-      <div>
-      <h3>Content of main App component</h3>
-      <ul>
-        <li><Link to="/one">One</Link></li>
-        <li><Link to="/two">Two</Link></li>
-        <li><Link to="/three">Three</Link></li>
-        <li><Link to="/four">Four</Link></li>
-      </ul>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-      <Switch>
-        <RouteWrapper path="/one" component={One} layout={LayoutOne} />
-        <RouteWrapper path="/two" component={Two} layout={LayoutOne} />
-        <RouteWrapper path="/three" component={Three} layout={LayoutTwo} />
-        <RouteWrapper path="/four" component={Four} layout={LayoutTwo} />
-      </Switch>
-      </div>
+      </header>
+      <body>
+        <div class="leftbox">
+          <Switch>
+            <Route path="/" component={Home}/>
+            <Route path="/profiles" component={Profiles}/>
+          </Switch>
+        </div>
+        <div class="leftbox">
+          <Switch>
+            <Route path="/" component={Home}/>
+            <Route path="/profiles" component={Profiles}/>
+          </Switch>
+        </div>
+      </body>
     </Router>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
 function RouteWrapper({
   component: Component, 
   layout: Layout, 
