12acca99bb50fd815f6f26386d5b79bff84e1514
Zhehai Zhang <zhehaizhang1@gmail.com>
2021-06-25 23:18:45 -0400

    switch to js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,38 +6,29 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import { LayoutOne, LayoutTwo } from './layouts/layouts';
+import { One, Two, Three, Four } from './components/components';
 
 export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+      <h3>Content of main App component</h3>
+      <ul>
+        <li><Link to="/one">One</Link></li>
+        <li><Link to="/two">Two</Link></li>
+        <li><Link to="/three">Three</Link></li>
+        <li><Link to="/four">Four</Link></li>
+      </ul>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+      <Switch>
+        <RouteWrapper path="/one" component={One} layout={LayoutOne} />
+        <RouteWrapper path="/two" component={Two} layout={LayoutOne} />
+        <RouteWrapper path="/three" component={Three} layout={LayoutTwo} />
+        <RouteWrapper path="/four" component={Four} layout={LayoutTwo} />
+      </Switch>
       </div>
     </Router>
   );
@@ -53,4 +44,17 @@ function About() {
 
 function Users() {
   return <h2>Users</h2>;
+}
+function RouteWrapper({
+  component: Component, 
+  layout: Layout, 
+  ...rest
+}) {
+  return (
+    <Route {...rest} render={(props) =>
+      <Layout {...props}>
+        <Component {...props} />
+      </Layout>
+    } />
+  );
 }
