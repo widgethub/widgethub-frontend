200850d945a52ad468a7aeb3856cbd20be07fe32
Frank Tao <48634762+kugelblitz2@users.noreply.github.com>
2021-06-26 02:52:03 -0400

    Finished main page template

--- src/App.js
+++ src/App.js
@@ -27,28 +27,22 @@ export default function App() {
         <img class="headelement logo" src={Logo} alt="logo"></img>
         <div class="headelement modetoggle bg-gray-200 text-sm text-gray-500 leading-none border-2 border-gray-200 rounded-full inline-flex">
           <button class="inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 focus:text-blue-400 rounded-l-full px-4 py-2 active" id="grid">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="fill-current w-4 h-4 mr-2"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
-            <span>Grid</span>
+            <span>Home</span>
           </button>
           <button class="inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 focus:text-blue-400 rounded-r-full px-4 py-2" id="list">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="fill-current w-4 h-4 mr-2"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3.01" y2="6"></line><line x1="3" y1="12" x2="3.01" y2="12"></line><line x1="3" y1="18" x2="3.01" y2="18"></line></svg>
-            <span>List</span>
+            <span>Profiles</span>
           </button>
         </div>
 
       </header>
       <body>
-        <div class="leftbox">
-          <Switch>
-            <Route exact path="/" >
-              <Home/>
-            </Route>
-            <Route exact path="/profiles" >
-              <Profiles/>
-            </Route>
-          </Switch>
+        <div class="content">
+          <switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/profiles" component={Profiles}></Route>
+          </switch>
         </div>
-     </body>
+      </body>
     </Router>
   );
 }
