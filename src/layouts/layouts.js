12054d1c02857e376aeb2bc4e52a0dd01583b0d0
Frank Tao <48634762+kugelblitz2@users.noreply.github.com>
2021-06-26 19:28:50 -0400

    added header buttons

--- src/layouts/layouts.js
+++ src/layouts/layouts.js
@@ -8,20 +8,11 @@ import { getToken } from '../services/auth.service';
 
 
 import Logo from '../assets/logo.png'
-function HeaderView() {
-  const location = window.location.pathname;
-  console.log(location)
-  if (location === "/") {
-    return <Link to="/profiles">Profiles</Link>
-  }
-  else if (location === "/profiles") {
-    return <Link to="/">Home</Link>
-  }
-  else {
-    return <Link to="/login">Login</Link>
-  }
-}
 
+function removeToken(){
+  window.localStorage.removeItem("token")
+  window.location.reload()
+}
 
 export default function Layout({children}) {
 
@@ -40,12 +31,24 @@ export default function Layout({children}) {
     <div>
       <header>
         <img className="headelement logo" src={Logo} alt="logo"></img>
-        <div className="headelement modetoggle text-sm ">
+        <div className="headelement navbtn text-sm ">
+          <div className="mb-2">
+            <button onClick={removeToken} className="px-5 py-1 rounded-xl text-sm font-medium text-indigo-600 bg-white outline-none focus:outline-none m-1 hover:m-0 focus:m-0 border border-indigo-600 hover:border-4 focus:border-4 transition-all">
+              Logout
+            </button>
+          </div>
+        </div>
+        <div className="headelement navbtn text-sm ">
           <div className="mb-2">
             <button className="px-5 py-1 rounded-xl text-sm font-medium text-indigo-600 bg-white outline-none focus:outline-none m-1 hover:m-0 focus:m-0 border border-indigo-600 hover:border-4 focus:border-4 transition-all">
-            {
-              HeaderView()
-            }
+            { <Link to="/profiles">Profiles</Link> }
+           </button>
+          </div>
+        </div>
+        <div className="headelement navbtn text-sm ">
+          <div className="mb-2">
+            <button className="px-5 py-1 rounded-xl text-sm font-medium text-indigo-600 bg-white outline-none focus:outline-none m-1 hover:m-0 focus:m-0 border border-indigo-600 hover:border-4 focus:border-4 transition-all">
+            { <Link to="/">Home</Link> }
            </button>
           </div>
         </div>
