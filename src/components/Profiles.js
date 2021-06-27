e579edadead7ac9e8013df1469a83ecca68e42e0
Zhehai Zhang <zhehaizhang1@gmail.com>
2021-06-27 04:54:59 -0400

    changes

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -194,7 +194,7 @@ export default function Profiles() {
                 </label>
                 <div className="relative">
                   <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
-                  id="grid-type" value={typeSelected} onChange={e => (
+                  value={typeSelected} onChange={e => (
                     changeTypeLink(e.target.value)
                     )} >
                     {types.map(function(type, i) {
