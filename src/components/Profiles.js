88a277e8c59cc1fb56d1efcecaabec118ab2356c
Frank Tao <48634762+kugelblitz2@users.noreply.github.com>
2021-06-26 22:11:24 -0400

    Major cosmetic improvements

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -113,6 +113,7 @@ export default function Profiles() {
 
   const removeProfile = async (index) => {
     await deleteProfile(profiles[index].id);
+    setProfiles(profiles.filter(profile => profile.id != profiles[index].id))
   }
 
   return(
@@ -123,21 +124,18 @@ export default function Profiles() {
           <ul className="divide-y divide-gray-300">
             {profiles.map(function(profile, i) {
               return (
-                <li key={i} class="p-4 hover:bg-gray-50 cursor-pointer" onClick={() => editProfile(i)}>
+                <li key={i} className="p-4 hover:bg-gray-50 cursor-pointer" onClick={() => editProfile(i)}>
                   {profile.name}
                 </li>
               )
             })}
           </ul>
         </div>
-        <button className="bg-white" onClick={newProfile}>
-          add
-        </button>
       </div>
 
       <div>
         <div className="grid bg-white shadow-xl rounded-lg mt-6 place-items-center">
-        <form className="w-5/6 max-w-md p-6">
+        <form className="w-full max-w-md p-6">
           <p className="text-4xl">Profile</p>
 
           <div className="flex flex-wrap -mx-3 mb-6 mt-6">
@@ -148,31 +146,23 @@ export default function Profiles() {
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-title" type="string" placeholder="Enter the title of your profile"
               onChange={e => setName(e.target.value)} value={name}/>
-              <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+              <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
             </div>
           </div>
 
           <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <div className="w-full px-3 mb-6 md:mb-0">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                First
+                Display Name
               </label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-              id="grid-first-name" type="text" placeholder="Jane"
+              id="grid-first-name" type="text" placeholder="fairnightzz"
               onChange={e => setFirst(e.target.value)} value={first}/>
-            </div>
-            <div className="w-full md:w-1/2 px-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                Last Name
-              </label>
-              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
-              id="grid-last-name" type="text" placeholder="Doe"
-              onChange={e => setLast(e.target.value)} value={last}/>
             </div>
           </div>
 
           <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <div className="w-full px-3 mb-6 md:mb-0">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
                 Type
               </label>
@@ -189,12 +179,6 @@ export default function Profiles() {
                   <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
                 </div>
               </div>
-            </div>
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-                Link
-              </label>
-              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-link" type="text" placeholder="To Profile"/>
             </div>
           </div>
 
@@ -217,10 +201,15 @@ export default function Profiles() {
 
           <button onClick={submitProfile} className="bg-white text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
               <span className="mr-2">{formState ? <p>Update</p> : <p>Add</p>}</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path fill="currentcolor" d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
-            </svg>
-          </button> 
+          </button>
+
+          <button onClick={newProfile} className="bg-white text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+              <span className="mr-2">Clear</span>
+          </button>
+
+          <button onClick={removeProfile} className="bg-white text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+              <span className="mr-2">Delete</span>
+          </button>
 
         </form>
 
